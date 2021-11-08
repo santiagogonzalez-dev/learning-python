827a267f3e76a9c97cd64d7932c3e28949e1143e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-11-08 19:38:45 -0300

    Updates with basic exercises

--- basic_exercises.py
+++ basic_exercises.py
@@ -1,140 +1,140 @@
-# # Multiply without using asterisk
+# Multiply without using asterisk
 
-# # num1 = int(input('Enter first number ->'))
-# # num2 = int(input('Enter second number ->'))
-# num1 = 9
-# num2 = 3
+# num1 = int(input('Enter first number ->'))
+# num2 = int(input('Enter second number ->'))
+num1 = 9
+num2 = 3
 
-# a = 9
-# b = 3
-# result_for = 0
-# for x in range(a):
-#     result_for += b
+a = 9
+b = 3
+result_for = 0
+for x in range(a):
+    result_for += b
 
-# print('Result ->', result_for)
-
-
-# # Insert name and lastname and print them backwards
-
-# name_user = 'Santiago'
-# lastname_user = 'Gonzalez'
-
-# concatenated = name_user + ' ' + lastname_user
-
-# print(concatenated[::-1])
+print('Result ->', result_for)
 
 
-# # Find the smallest element from a list
+# Insert name and lastname and print them backwards
 
-# numbers_list = [ 1, 2, 5, 3, 55, -24, -13 ]
+name_user = 'Santiago'
+lastname_user = 'Gonzalez'
 
-# menor = 'init'
+concatenated = name_user + ' ' + lastname_user
 
-# for x in numbers_list:
-#     if menor == 'init':
-#         menor = x
-#     else:
-#         menor = x if x < menor else menor
+print(concatenated[::-1])
 
 
-# # Create a function that returns the volume of a sphere with the radius
+# Find the smallest element from a list
 
-# import math
+numbers_list = [ 1, 2, 5, 3, 55, -24, -13 ]
 
-# # radius = int(input('Give the radius of the sphere ==>'))
-# radius = 3
+menor = 'init'
 
-# # Formula: 4/3 x pi x (diameter /2)^3
-# def volume_of_sphere(r):
-#     return (4/3 * math.pi * (r) ** 3)
-
-
-# print(volume_of_sphere(radius))
+for x in numbers_list:
+    if menor == 'init':
+        menor = x
+    else:
+        menor = x if x < menor else menor
 
 
-# # Write a function the tells if the user is of legal age
+# Create a function that returns the volume of a sphere with the radius
 
-# def user_legale_age(usuario):
-#     return usuario.edad > 17
+import math
 
-# class Usuario:
-#     def __init__(self, edad):
-#         self.edad = edad
+# radius = int(input('Give the radius of the sphere ==>'))
+radius = 3
 
-# usuario = Usuario(15)
-# usuario2 = Usuario(21)
-
-# resultado1 = user_legale_age(usuario)
-# resultado2 = user_legale_age(usuario2)
-
-# print(resultado1, resultado2)
-# print()
+# Formula: 4/3 x pi x (diameter /2)^3
+def volume_of_sphere(r):
+    return (4/3 * math.pi * (r) ** 3)
 
 
-# # Check if a number is odd(impar) or even(par)
-
-# def odd_even(num):
-#     if num%2 == 0:
-#         return print('Number is even')
-#     elif num%2 == 1:
-#         return print('Number is odd')
-
-# # odd_even(6)
-# odd_even(10)
-# print()
+print(volume_of_sphere(radius))
 
 
-# # Write a function that indicates how many vowels a word has
+# Write a function the tells if the user is of legal age
 
-# def quantity_vowels(word):
-#     count = 0
-#     # for c in word:
-#     #     print('Current character ->', c)
-#     #     if c == 'a':
-#     #         count += 1
-#     #     elif c == 'e':
-#     #         count += 1
-#     #     elif c == 'i':
-#     #         count += 1
-#     #     elif c == 'o':
-#     #         count += 1
-#     #     elif c == 'u':
-#     #         count += 1
-#     # OR DO THIS THAT IS CLEANER
-#     for c in word:
-#         if c in 'aeiou':
-#             count += 1
+def user_legale_age(usuario):
+    return usuario.edad > 17
 
-#     print('There is', count, 'vowels in this word')
+class Usuario:
+    def __init__(self, edad):
+        self.edad = edad
+
+usuario = Usuario(15)
+usuario2 = Usuario(21)
+
+resultado1 = user_legale_age(usuario)
+resultado2 = user_legale_age(usuario2)
+
+print(resultado1, resultado2)
+print()
 
 
-# word = 'Chanchito'.lower()
-# quantity_vowels(word)
+# Check if a number is odd(impar) or even(par)
+
+def odd_even(num):
+    if num%2 == 0:
+        return print('Number is even')
+    elif num%2 == 1:
+        return print('Number is odd')
+
+# odd_even(6)
+odd_even(10)
+print()
 
 
-# # Write an application that receives an infinite quantity of numbers until the user types
-# # "stop", for later receive the sum of entered numbers.
+# Write a function that indicates how many vowels a word has
 
-# respuesta = '0'
-# final_result = 0
-# while respuesta != 'stop':
-#     respuesta = input('Enter numbers to sum: ')
-#     if respuesta == 'stop':
-#         print('User input: stop')
-#     else:
-#         try:
-#             respuesta_int = int(respuesta)
-#             final_result += respuesta_int
-#         except:
-#             print('The introduced character is not a number, try again')
+def quantity_vowels(word):
+    count = 0
+    # for c in word:
+    #     print('Current character ->', c)
+    #     if c == 'a':
+    #         count += 1
+    #     elif c == 'e':
+    #         count += 1
+    #     elif c == 'i':
+    #         count += 1
+    #     elif c == 'o':
+    #         count += 1
+    #     elif c == 'u':
+    #         count += 1
+    # OR DO THIS THAT IS CLEANER
+    for c in word:
+        if c in 'aeiou':
+            count += 1
 
-# print('The total is ==>', final_result)
+    print('There is', count, 'vowels in this word')
 
 
-# # Write a function that receives name and lastname that add thems to a file
+word = 'Chanchito'.lower()
+quantity_vowels(word)
 
-# def write_name(name_user, lastname_user):
-#     with open('list_names', 'a') as l:  # Write mode
-#         l.write("%s %s\n" % (name_user, lastname_user))
 
-# write_name(input('Enter name'), input('Enter last name'))
+# Write an application that receives an infinite quantity of numbers until the user types
+# "stop", for later receive the sum of entered numbers.
+
+respuesta = '0'
+final_result = 0
+while respuesta != 'stop':
+    respuesta = input('Enter numbers to sum: ')
+    if respuesta == 'stop':
+        print('User input: stop')
+    else:
+        try:
+            respuesta_int = int(respuesta)
+            final_result += respuesta_int
+        except:
+            print('The introduced character is not a number, try again')
+
+print('The total is ==>', final_result)
+
+
+# Write a function that receives name and lastname that add thems to a file
+
+def write_name(name_user, lastname_user):
+    with open('list_names', 'a') as l:  # Write mode
+        l.write("%s %s\n" % (name_user, lastname_user))
+
+write_name(input('Enter name'), input('Enter last name'))
